caa162ac43544c344b4d1b28688d275195b5d9c8
Sany <human.sany7@gmai.com>
2025-05-08 22:01:30 +0600

    second

--- lec-2/string.py
+++ lec-2/string.py
@@ -12,3 +12,9 @@ print(ch)
 
 print(str1[0:4])
 print(str2[30 : len(str2)])
+
+print(str1.endswith("ing"))
+print(str1.endswith("xyz"))
+
+print(str1.replace("string", "String"))
+print(str1.find("s"))
