e6e4dc0bf1447b7621a2f4c59c3224f83b4851d4
Sany <human.sany7@gmai.com>
2025-05-08 21:52:27 +0600

    first

---
+++ lec-2/string.py
@@ -0,0 +1,14 @@
+str1 = "this is a string"
+str2 = "  also a string \n this is new line"
+print(str1)
+print(str2)
+
+
+print(str1 + str2)
+print(len(str2))
+
+ch = str2[7]
+print(ch)
+
+print(str1[0:4])
+print(str2[30 : len(str2)])
