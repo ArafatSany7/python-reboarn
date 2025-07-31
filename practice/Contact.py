7aa15bbe2a1933c2f63d4ce18adad3d25ea91586
Sany <human.sany7@gmail.com>
2025-08-01 02:17:46 +0600

    algo6

---
+++ practice/Contact.py
@@ -0,0 +1,19 @@
+conact = ["Alice", "Bob", "Charlie", "David"]
+
+find = input("Enter name : ")
+
+
+def ls(arr, key):
+    for i, item in enumerate(arr):
+        if item == key:
+            return i
+
+    return -1
+
+
+index = ls(conact, find)
+
+if index != -1:
+    print(f"contact found at index :{index}")
+else:
+    print("Index not found")
