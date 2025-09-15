897d8f9088c3f29459a70456535a7217b5d8669e
ArafatSany7 <human.sany7@gmail.com>
2025-09-16 00:49:51 +0600

    tempa

--- kilo.py
+++
@@ -1,5 +0,0 @@
-Kilometer = float(input("Enter distance in Kilometer : "))
-convert = 0.621371
-mile = convert * Kilometer
-
-print(f"{Kilometer} Kilometer is equal to {mile} mile")
---
+++ practice/tempa.py
@@ -0,0 +1,5 @@
+celsius = float(input("Enter temparature in Celsius : "))
+
+farenhite = (celsius * 9 / 5) + 32
+
+print(f"{celsius} Celsius is equals to {farenhite}  farenhite")
