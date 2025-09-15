c87e3cfcd05717c710d90bd53bc07156bf674ca2
ArafatSany7 <human.sany7@gmail.com>
2025-09-16 02:35:57 +0600

    prime_range

---
+++ practice/prime_range.py
@@ -0,0 +1,11 @@
+low = 1
+up = 10
+
+print("Prime numbers between", low, "and", up, "are:")
+for num in range(low, up + 1):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
